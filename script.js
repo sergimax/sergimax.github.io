65430a5e1c3b24b29f2e6481abd431a413a75956
Sergienko Maxim <creativesun@yandex.ru>
2023-08-22 12:19:01 +0300

    feat() Employment basic info

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let employmentSection = document.getElementById("employment");
+
+let employmentHeader = document.createElement("h3")
+employmentHeader.textContent = "Работа";
+employmentSection.appendChild(employmentHeader);
+
+let employmentDescription = document.createElement("p")
+employmentDescription.textContent = "На данный момент являюсь разработчиком в R-Style Softlab";
+employmentSection.appendChild(employmentDescription);
